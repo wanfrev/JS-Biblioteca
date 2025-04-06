61d93371c8a585d56917247cdfe1151540d69c33
Wanfredo Vargas <wanfrevargas@gmail.com>
2025-04-05 22:43:30 -0400

    Cambios header, sidebar y tesis

--- Frontend/src/js/app.js
+++ Frontend/src/js/app.js
@@ -69,7 +69,7 @@ function displayThesisDetails(tesis) {
     <p><strong>Autor:</strong> ${tesis.autor || "Desconocido"}</p>
     <p><strong>Carrera:</strong> ${tesis.carrera || "Sin carrera"}</p>
     <p><strong>Fecha de Publicación:</strong> ${fechaFormateada}</p>
-    <p><strong>Descripción:</strong> ${tesis.des_tesis}</p>
+    <p><strong>Descripción:</strong> ${tesis.des_tesis || "Sin descripción"}</p>
     <a href="http://localhost:5000/upload/${tesis.documento}" target="_blank">Ver PDF</a>
   `;
 }
